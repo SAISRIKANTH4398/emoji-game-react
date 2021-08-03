f1ed6cf534a8abcb46725f5f9aa436db045e7a1e
saisrikanth4398 <sreekanthkasavajjula@gmail.com>
2021-08-03 11:13:38 +0000

    Emoji game using react

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const EmojiCard = props => {
+  const {emoji, onClickEmoji} = props
+  const {id, emojiName, emojiUrl} = emoji
+  const onClickEmojiCard = () => {
+    onClickEmoji(id)
+  }
+
+  return (
+    <li className="emoji-item" onClick={onClickEmojiCard}>
+      <img src={emojiUrl} className="emoji-icon" alt={emojiName} />
+    </li>
+  )
+}
+
+export default EmojiCard
